264005fa78db754a8c69d3a47f75c88afecb3f9a
Azmi <azmi@embreo.global>
2022-11-02 16:05:16 +0000

    start scaffolding

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import logo from "./assets/logo-big.png";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const TopBar = () => {
+  return (
+    <div className="flex justify-between items-center bg-gray-200 py-2 px-12 fixed top-0 left-0 right-0 h-24">
+      <div className="flex items-center">
+        <GiHamburgerMenu className="text-2xl mr-44 cursor-pointer" />
+        <img
+          src={logo}
+          alt="logo"
+          className="h-24 cursor-pointer transition-all delay-200 hover:scale-105"
+        />
+      </div>
+      <div className="flex items-center">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">
+          Login
+        </button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const SideBar = () => {
+  return (
+    <div className="bg-gray-200 w-64 h-screen fixed top-24 left-0 bottom-0"></div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className='text-5xl uppercase'>Hello</h1>
-    </div>
-  )
+    <main>
+      <TopBar />
+      <SideBar />
+    </main>
+  );
 }
 
-export default App
+export default App;
