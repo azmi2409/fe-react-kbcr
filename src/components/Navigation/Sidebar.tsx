141ba997f54e6f6ea65f6ab55e398e70b4c8d674
Azmi <azmi@embreo.global>
2022-12-04 11:21:09 +0000

    add sidebar

--- src/components/Navigation/Sidebar.tsx
+++ src/components/Navigation/Sidebar.tsx
@@ -1,16 +1,56 @@
-import { Navbar } from "@mantine/core";
+import { Navbar, NavLink } from "@mantine/core";
+import { MdOutlineDashboard } from "react-icons/md";
+import { SiManageiq } from "react-icons/si";
+import { TbReportAnalytics } from "react-icons/tb";
+import { IoSettingsOutline } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
+
+const menus = [
+  {
+    title: "Dashboard",
+    icon: MdOutlineDashboard,
+    link: "/",
+  },
+  {
+    title: "Management",
+    icon: SiManageiq,
+    link: "/management",
+  },
+  {
+    title: "Reports",
+    icon: TbReportAnalytics,
+    link: "/reports",
+  },
+  {
+    title: "Settings",
+    icon: IoSettingsOutline,
+    link: "/settings",
+  },
+];
 
 const SideBar = () => {
+  const navigate = useNavigate();
   return (
     <Navbar
       width={{
-        base: 300,
+        base: 200,
       }}
       height="100vh"
       p="xs"
       className="transition-all delay-200 bg-gray-100"
     >
-      test
+      <Navbar.Section grow mt="sm" p="md">
+        <div className="flex flex-col gap-5">
+          {menus.map((menu) => (
+            <NavLink
+              key={menu.title}
+              icon={<menu.icon />}
+              label={menu.title}
+              onClick={() => navigate(menu.link)}
+            />
+          ))}
+        </div>
+      </Navbar.Section>
     </Navbar>
   );
 };
